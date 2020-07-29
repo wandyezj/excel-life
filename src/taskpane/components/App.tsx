110aef689d76aab194f1d0932113bdc74548964d
wandyezj <wandyezj@gmail.com>
2020-07-29 12:41:37 -0700

    make react react

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -3,21 +3,48 @@ import { Button, ButtonType } from "office-ui-fabric-react";
 import Header from "./Header";
 import List, { ListItem } from "./List";
 import Progress from "./Progress";
+import Select from "./Select";
 
 export interface AppProps {
   title: string;
   isOfficeInitialized: boolean;
 }
 
+// All state must be stored as part of the global state of the application
 export interface AppState {
   listItems: ListItem[];
+
+  /**
+   * Yearly Job Income
+   */
+  income: number;
 }
+
+interface Occupation {
+  /**
+   * name of the occupation
+   */
+  name:string;
+
+  /**
+   * Yearly Income
+   */
+  income:number;
+}
+
+const occupations: Occupation[] =[
+  {name:"None", income: 0}
+  {name:"Low", income:20000},
+  {name:"Medium", income:60000},
+  {name:"High", income:100000},
+]
 
 export default class App extends React.Component<AppProps, AppState> {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      listItems: []
+      listItems: [],
+      income: 0,
     };
   }
 
@@ -78,6 +105,17 @@ export default class App extends React.Component<AppProps, AppState> {
           <p>
             Modify the source files, then click <b>Run</b>.
           </p>
+          <h2>Occupation Income</h2>
+          <Select id="test" options={
+            occupations.map(({name}) => name)
+          }
+          onChange={(index: number) => {
+            this.setState({income:occupations[index].income});
+          }}
+            />
+          <br/>
+        <p>{this.state.income}</p>
+        <br/>
           <Button
             className=""
             buttonType={ButtonType.hero}
